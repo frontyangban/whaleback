72bde36047241f14f3aefac5a9f42390b031001a
yangban <frontyangban@gmail.com>
2025-04-15 22:21:35 +0900

    change environment variables loading

--- host/vite.config.ts
+++ host/vite.config.ts
@@ -1,56 +1,50 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 import path from 'path'
 
-const getRemoteEntryUrl = (remoteName: string) => {
-  const remoteUrl =
-    process.env[`VITE_APP_REMOTE_${remoteName.toUpperCase()}_URL`]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
 
-  if (!remoteUrl) {
-    // TODO port mapper
-    return `http://localhost:5000/assets/remoteEntry.js`
-  }
-
-  return remoteUrl
-}
-
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'host',
-      remotes: {
-        // first/App
-        first: getRemoteEntryUrl('first'),
-      },
-      shared: {
-        react: {
-          requiredVersion: '^18.2.0',
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: 'host',
+        remotes: {
+          // first/App
+          first:
+            mode === 'production'
+              ? env.VITE_APP_REMOTE_FIRST_URL
+              : 'localhost:5000/assets/remoteEntry.js',
         },
-        'react-dom': {
-          requiredVersion: '^18.2.0',
+        shared: {
+          react: {
+            requiredVersion: '^18.2.0',
+          },
+          'react-dom': {
+            requiredVersion: '^18.2.0',
+          },
+          'react-router-dom': {
+            requiredVersion: '^6.22.2',
+          },
         },
-        'react-router-dom': {
-          requiredVersion: '^6.22.2',
-        },
-      },
-    }),
-  ],
-  envDir: 'env',
-  server: {
-    port: 5000,
-    strictPort: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+      }),
+    ],
+    server: {
+      port: 5000,
+      strictPort: true,
     },
-  },
-  build: {
-    modulePreload: false,
-    target: 'esnext',
-    minify: false,
-    cssCodeSplit: false,
-  },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    build: {
+      modulePreload: false,
+      target: 'esnext',
+      minify: false,
+      cssCodeSplit: false,
+    },
+  }
 })
